4d4932792f1c554d3f8ac42b48985095d0b9b975
Terry Ott <Terry.Ott@sixbuckssolutions.com>
2026-07-03 14:15:47 +0000

    Checking cookie and query param for user ID

--- src/watches/js/index.js
+++ src/watches/js/index.js
@@ -1,1 +1,49 @@
-console.log("JS is loaded");
+function cookieValue(cookieName) {
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const cookieArray = decodedCookie.split(';');
+    const searchString = cookieName + "=";
+
+    for (let i = 0; i < cookieArray.length; i++) {
+        let cookie = cookieArray[i].trim();
+        if (cookie.indexOf(searchString) === 0) {
+            return cookie.substring(SearchString.length, cookie.length);
+        }
+    }
+
+    return null;
+}
+
+
+function queryParam() {
+    return null;
+}
+
+
+function userId() {
+    // Check cookie
+    let userId = cookieValue("__Secure-user-id")
+
+    if (userId === null) {
+        console.log("UserID not found in cookie, checking query param")
+        userId = queryParam("user_id")
+    }
+
+    // STILL didn't find user ID
+    if (userId === null) {
+        // Redirect to login 
+        console.log("Redirecting to login page; no user ID in cookies or query param")
+    }
+
+    return userId;
+}
+
+
+function main() {
+    console.log("JS is loaded");
+    const userId = userId();
+    if (userId !== null) {
+        console.log("User is logged in with user ID \"" + userId + "\"");
+    }
+}
+
+main();
