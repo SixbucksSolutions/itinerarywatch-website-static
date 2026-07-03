9222b0ffa72170fc14f06ec67fcad1d9bdbd04df
Terry Ott <Terry.Ott@sixbuckssolutions.com>
2026-07-03 17:26:51 +0000

    Called nonexistent function

--- src/watches/js/api.js
+++ src/watches/js/api.js
@@ -1,4 +1,4 @@
-function apiUserMailAddress() {
+function apiUserEmailAddress() {
     console.log("TODO: redirect user to login page; backend threw a 401 saying auth creds are missing or malformed")
     return null;
 }
