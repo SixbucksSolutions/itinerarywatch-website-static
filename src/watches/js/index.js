e3bdcc0f7e2ee67ba6a6fc7992c47d18aefc44a4
Terry Ott <Terry.Ott@sixbuckssolutions.com>
2026-07-03 20:01:59 +0000

    JS debugging sucks

--- src/watches/js/index.js
+++ src/watches/js/index.js
@@ -1,12 +1,12 @@
 console.log("Watches JS starting")
 
-/*
 const startTime = performance.now();
 
-const userInfo = await apiUserInfo();
+//const userInfo = await apiUserInfo();
 
 const endTime = performance.now();
 const duration = endTime - startTime;
 
-console.log(`User info for successfully-authenticated user ${userInfo.email_address} retrieved from API in ${duration.toFixed(3)} ms`);
-*/
+// console.log(`User info for successfully-authenticated user ${userInfo.email_address} retrieved from API in ${duration.toFixed(3)} ms`);
+
+console.log("End of script")
