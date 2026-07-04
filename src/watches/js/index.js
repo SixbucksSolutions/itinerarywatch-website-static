395279e892de3ef22df6b6ef145c125f4c00ab8d
Terry Ott <Terry.Ott@sixbuckssolutions.com>
2026-07-04 17:30:31 +0000

    Seeing if we're ready to render

--- src/watches/js/index.js
+++ src/watches/js/index.js
@@ -1,24 +1,43 @@
+let userInfo = null;
+let userWatches = null;
+
+
+function renderUserSpecificDataIfReady() {
+
+    // Bail out if we're not, in fact, ready to render
+    if ((userInfo === null) || (userWatches === null)) {
+        return;
+    }
+
+    console.log("As all required data has been successfully data, now populating hidden part of pag ewit huser-specific data");
+
+}
+
 async function getUserInfo() {
     const startTime = performance.now();
 
-    const userInfo = await apiUserInfo();
+    userInfo = await apiUserInfo();
 
     const endTime = performance.now();
     const duration = Math.ceil(endTime - startTime);
 
     console.log(`Userinfo retrieved for ${userInfo.email_address} in ${duration} ms`);
+
+    renderUserSpecificDataIfReady();
 }
 
 
 async function getUserWatches() {
     const startTime = performance.now();
 
-    const userWatches = await apiUserWatches();
+    userWatches = await apiUserWatches();
 
     const endTime = performance.now();
     const duration = Math.ceil(endTime - startTime);
 
     console.log(`User's itinerary watches retrieved in ${duration} ms`);
+
+    renderUserSpecificDataIfReady();
 }
 
 
