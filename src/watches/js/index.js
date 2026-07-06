c45b5dc3457697cd09a75553d4d5bcd4182cd425
Terry Ott <Terry.Ott@sixbuckssolutions.com>
2026-07-06 17:48:33 +0000

    Starting skeleton for search details

--- src/watches/js/index.js
+++ src/watches/js/index.js
@@ -88,7 +88,9 @@ function renderSingleUserWatchDetails() {
         const hiddenDataRenderDuration = Math.ceil(hiddenDataRenderTime - pageStartTime);
         console.log(`Making dynamic content for single watch visible ${hiddenDataRenderDuration} ms after API queries sent in parallel`);
     }
-    displayFatalError("Render single user watch details not implemented yet!");
+    document.getElementById("div_id_dynamic_data_single_search").style.display = "block";
+
+    console.log("Set single watch search dynamic data to visible");
 }
 
 async function getUserInfo() {
